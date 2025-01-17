e00c1ae1a75614f73f3c6e32eafa3e1425efe1b5
Chandra6388 <cppatel6388@gmail.com>
2025-01-17 08:47:31 +0530

    dfff

---
+++ Backend/App/Routes/Auth/authRoutes.jsx
@@ -0,0 +1,17 @@
+
+"use strict"
+
+const router = require("express").Router()
+// const {verifyToken}= require('../../Middlewares/autt.middleware')
+
+// const { login,verifyUser,logoutUser,ForgetPassword,ResetPassword,UpdatePassword , SignUpUser} = require('../../Controllers/Auth/Auth.controller')
+
+
+
+ 
+
+
+
+module.exports = router;
+
+
---
+++ Backend/app.jsx
@@ -0,0 +1,18 @@
+require('dotenv').config();
+const mongooseConnection  = require('./App/Connection/mongooseConnection')
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json({limit: '50mb', extended: true}));
+app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
+require('./App/Routes')(app);
+
+server.listen(process.env.PORT, () => {
+    console.log(`Server is running on port ${process.env.PORT}`);
+    mongooseConnection();
+});
