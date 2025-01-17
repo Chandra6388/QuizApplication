73da7904da74486d2e43e0e38816e92fac8d09fe
Chandra6388 <cppatel6388@gmail.com>
2025-01-17 08:52:57 +0530

    add initial backend dependencies and module files

--- Backend/app.js
+++ Backend/app.js
@@ -12,7 +12,10 @@ app.use(bodyParser.json({limit: '50mb', extended: true}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
 require('./App/Routes')(app);
 
+
+
+console.log('process.env.PORT', process.env.PORT);
 server.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
-    mongooseConnection();
+    // mongooseConnection();
 });
