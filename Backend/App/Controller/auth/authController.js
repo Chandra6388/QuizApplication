c0940588f816b443aee679bf8e2cf31a9e1804c4
Chandra6388 <cppatel6388@gmail.com>
2025-01-18 12:25:12 +0530

    add initial backend models and authentication controller setup

---
+++ Backend/App/Controller/auth/authController.js
@@ -0,0 +1,37 @@
+"use strict";
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+
+ 
+const db = require('../../Models');
+const SignUpUser = db.SignUpUser;
+const User = db.user;
+ 
+
+
+// Login CLASS
+class Auth {
+
+    // Login User
+    async login(req, res) {
+       
+
+    }
+
+
+  
+
+
+
+   
+
+
+  
+
+ 
+
+
+}
+
+
+module.exports = new Auth();
---
+++ Backend/App/Modals/user.model.js
@@ -0,0 +1,51 @@
+const { Schema, model } = require('mongoose'); 
+
+const userModel = Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    role: {
+        type: String,
+        enum: ['ADMIN', 'TEACHER', 'STAFF', 'USER'],
+        default: 'USER',
+        required: true
+    },
+    lastLogin: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        enum: ['active', 'inactive', 'suspended'],
+        default: 'active'
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    },
+    updatedAt: {
+        type: Date,
+        default: Date.now
+    }
+},
+    {
+        timestamps: true
+    },
+
+)
+const User_model = model('USER', userModel);
+
+
+
+module.exports = User_model;
