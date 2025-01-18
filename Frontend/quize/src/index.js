b2165e80947e3eee30060078663254396a74ad57
Chandra6388 <cppatel6388@gmail.com>
2025-01-18 11:20:45 +0530

    add initial vendor assets and plugin files for chart.js and tinymce

--- Frontend/quize/src/index.js
+++ Frontend/quize/src/index.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { HashRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 import App from './App';
-import { HashRouter } from "react-router-dom";
-import { Provider } from 'react-redux'
-import Store from "./ReduxStore/Store/Store";
+import Store from './ReduxStore/Store/Store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-
   <HashRouter>
     <Provider store={Store}>
       <App />
     </Provider>
   </HashRouter>
-
-
 );
-
